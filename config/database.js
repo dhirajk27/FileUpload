f20735c5455ada773c0375d0b6b84531594fcc09
Dhiraj <dheerajkhandave9552@gmail.com>
2024-08-11 00:23:55 +0530

    database

--- config/database.js
+++ config/database.js
@@ -1,19 +1,17 @@
-const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 express.connect=()=> {
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-        .then(() => console.log("DB connected successfully"))
-        .catch((error)=>{
+    .then(() => console.log("DB connected successfully"))
+    .catch((error)=>{
             console.log("DB connection issues");
             console.error(error);
             process.exit(1);
         });
-
 };
 
 
