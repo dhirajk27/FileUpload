bf400ade6d7242c7d4cc2af357aec0622e3491dd
Dhiraj <dheerajkhandave9552@gmail.com>
2024-08-12 02:39:05 +0530

    database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,39 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// express.connect=()=> {
+//     mongoose.connect(process.env.MONGODB_URL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("DB connected successfully"))
+//     .catch((error)=>{
+//             console.log("DB connection issues");
+//             console.error(error);
+//             process.exit(1);
+//         });
+// };
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-express.connect=()=> {
+// Define a function to connect to MongoDB
+const connect = () => {
     mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
     .then(() => console.log("DB connected successfully"))
-    .catch((error)=>{
-            console.log("DB connection issues");
-            console.error(error);
-            process.exit(1);
-        });
+    .catch((error) => {
+        console.log("DB connection issues");
+        console.error(error);
+        process.exit(1);
+    });
 };
+
+// Export the connect function
+module.exports = { connect };
+
+
 
 
