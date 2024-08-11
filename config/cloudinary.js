0d84e1b93c816debb42aa273102f4fb40a791084
Dhiraj <dheerajkhandave9552@gmail.com>
2024-08-12 02:38:51 +0530

    cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -7,8 +7,10 @@ exports.cloudinaryConnect = () => {
             cloud_name:process.env.CLOUD_NAME,
             api_key:process.env.API_KEY,
             api_secret:process.env.API_SECRET,
-        })
+        });
+        console.log("Cloudinary connected successfully");
+        
     } catch(error) {
-        console.log(error);
+        console.log("Cloud connection error:",error);
     }
 }
