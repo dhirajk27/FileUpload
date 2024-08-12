396e28e503ec1c81b7b980a664024f78eb42be94
Dhiraj <dheerajkhandave9552@gmail.com>
2024-08-13 01:18:27 +0530

    added post middleware

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
     name: {
@@ -15,6 +16,37 @@ const fileSchema = new mongoose.Schema({
         type: String,
     },
 });
+// post middleware
+fileSchema.post("save",async function(doc){
+    try {
+        console.log("Doc",doc)
 
+        // transporter
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS,
+            },
+        });
+
+        // mail send 
+        let info = await transporter.sendMail({
+            from:``,
+            to:doc.email,
+            subject:"New file uploaded on cloudinary",
+            html: `
+            <h2>Hello,</h2>
+            <p>Your file has been uploaded successfully.</p>
+            <p>View it here: <a href="${doc.imageUrl}">${doc.name}</a></p>
+        `, 
+        })
+        console.log("Info",info);
+        
+
+    } catch (error) {
+        console.error(error);
+    }
+})
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
